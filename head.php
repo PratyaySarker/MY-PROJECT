71987758765bcd98c60ec1a568a120082d215f90
asmaulnijhum957 <74194559+asmaulnijhum957@users.noreply.github.com>
2021-01-11 15:36:37 +0600

    Add files via upload

--- head.php
+++ head.php
@@ -1,8 +1,5 @@
 	
 	<meta charset="UTF-8">
-		<title>Shopaholic BD</title>
+		<title>Online Bazar</title>
 		<link rel="stylesheet" href="css/bootstrap.min.css"/>
-		<script src="js/jquery2.js"></script>
-		<script src="js/bootstrap.min.js"></script>
-		<script src="main.js"></script>
 		<link href="style.css">
