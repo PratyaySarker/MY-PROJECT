8146aa5c927de7cf2d17ca955e321d64c963559f
asmaulnijhum957 <74194559+asmaulnijhum957@users.noreply.github.com>
2021-01-11 14:55:06 +0600

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Online Bazar</title>
+  <?php include 'head.php';  ?>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <style>
+    #HomeNav {
+    margin-left: 19%; 
+  }
+  .main{
+    flex: 70%;
+    background-color: white;
+    margin-left: 20%;
+  }
+  </style>
+</head>
+  
+
+<body>
+<?php include 'HeadNav.php'; ?>
+
+<?php include 'SideNav.php'; ?>
+
+
+<div class="main" style="margin-left: 25%"> 
+  <div class="contact">
+    <h2> Contact us: </h2>
+    <P>Asmaul Husna Nijhum, : <br>
+      <a href="mailto:asmaulnijhum957@gmail.com?Subject=Hello%20again" target="_top"><span class="glyphicon glyphicon-envelope"> asmaulnijhum957@gmail.com</span></a>  
+      <span class="glyphicon glyphicon-earphone">0190000000</span>
+    </P>
+
+  </div>
+</div>
+
+</body>
+<footer>
+  <div>
+    <p>&copy; All rights reserved by OnlineBazar </p>
+  </div>
+</footer>
+
+</html>
